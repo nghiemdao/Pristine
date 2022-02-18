21bfc895db6ad34ed4427b0a84342fd6254bbb72
nghiemdao <nghiemvandao@gmail.com>
2022-02-18 11:02:09 +0700

    Change default language to ja & add extra validations

--- src/lang.js
+++ src/lang.js
@@ -1,17 +1,19 @@
 export const lang = {
-    en: {
-        required: "This field is required",
-        email: "This field requires a valid e-mail address",
-        number: "This field requires a number",
-        integer: "This field requires an integer value",
-        url: "This field requires a valid website URL",
-        tel: "This field requires a valid telephone number",
-        maxlength: "This fields length must be < ${1}",
-        minlength: "This fields length must be > ${1}",
-        min: "Minimum value for this field is ${1}",
-        max: "Maximum value for this field is ${1}",
-        pattern: "Please match the requested format",
-        equals: "The two fields do not match",
-        default: "Please enter a correct value"
-    }
+  ja: {
+    required: "こちらの項目は必須項目です",
+    email: "有効なメールアドレスを入力してください",
+    number: "は数値で入力してください",
+    integer: "は整数で入力してください",
+    url: "URLが無効です",
+    tel: "This field requires a valid telephone number",
+    maxlength: "This fields length must be < ${1}",
+    minlength: "${1}文字以上の半角英数字を入力してください",
+    min: "は${1}より大きい値にしてください",
+    max: "Maximum value for this field is ${1}",
+    pattern: "Please match the requested format",
+    equals: "The two fields do not match",
+    "required-group": "こちらの項目は必須項目です",
+    "integer-group": "は整数で入力してください",
+    greater: "上限金額は下限金額よりも多く設定してください"
+  }
 };
